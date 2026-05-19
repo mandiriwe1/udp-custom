3a55feeb622b48103a23b5ef1a0f61567ce607bf
mandiriwe1 <mandiriwe1@gmail.com>
2026-05-19 17:52:19 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,12 +21,12 @@ echo "[+] Setting timezone Sri Lanka"
 ln -fs /usr/share/zoneinfo/Asia/Colombo /etc/localtime
 
 echo "[+] Download UDP-Custom"
-wget -q --show-progress "https://github.com/noobconner21/UDP-Custom-Script/raw/main/udp-custom-linux-amd64" -O /root/udp/udp-custom
+wget -q --show-progress "https://github.com/mandiriwe1/udp-custom/raw/main/udp-custom-linux-amd64" -O /root/udp/udp-custom
 
 chmod +x /root/udp/udp-custom
 
 echo "[+] Download config"
-wget -q --show-progress "https://raw.githubusercontent.com/noobconner21/UDP-Custom-Script/main/config.json" -O /root/udp/config.json
+wget -q --show-progress "https://raw.githubusercontent.com/mandiriwe1/udp-custom/main/config.json" -O /root/udp/config.json
 
 chmod 644 /root/udp/config.json
 
@@ -35,7 +35,7 @@ EXCLUDE="$1"
 echo "[+] Create systemd service"
 cat <<EOF > /etc/systemd/system/udp-custom.service
 [Unit]
-Description=UDP Custom Service (SSLAB MOD)
+Description=UDP Custom Service (yhds mod)
 After=network.target
 
 [Service]
@@ -57,10 +57,10 @@ systemctl restart udp-custom
 clear
 echo "[+] Installing UDP Manager"
 
-mkdir -p /etc/Sslablk
-cd /etc/Sslablk
+mkdir -p /etc/yhds
+cd /etc/yhds
 
-wget -q "https://github.com/noobconner21/UDP-Custom-Script/raw/main/system.zip" -O system.zip
+wget -q "https://github.com/mandiriwe1/udp-custom/raw/main/system.zip" -O system.zip
 
 unzip -o system.zip
 
@@ -72,7 +72,7 @@ chmod +x ChangeUser.sh Adduser.sh DelUser.sh Userlist.sh RemoveScript.sh torrent
 rm -f ../system.zip
 
 echo ""
-echo "UDP Install By SSLAB LK Dev Team"
+echo "UDP Install By Yhds LK Dev Team"
 echo "Support: GitHub/mandiriwe1"
 echo ""
 
